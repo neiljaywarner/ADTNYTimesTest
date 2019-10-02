1d861a8adec77d3001ba8003c82eb1f467ce2745
Neil Jay Warner <neilwarner@gmail.com>
2019-10-02 12:37:27 -0500

    Delete NYTArticlesEndPoint.java
    Sigh

--- app/src/main/java/com/neiljaywarner/adtnytimestest/NYTArticlesEndPoint.java
+++
@@ -1,9 +0,0 @@
-package com.neiljaywarner.adtnytimestest;
-
-/**
- * This is a file to not check into github.
- */
-public class NYTArticlesEndPoint {
-
-    public static final String APIKey = "15629235341919a7b4b8b6e9344f9bca:6:72095783";
-}
